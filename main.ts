e2686d87e90da9b9204fef73ca9dbe8e81a023a3
Max Tremaine <max.tremaine@gmail.com>
2023-03-12 11:59:29 -0400

    Added start conditio

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
 import { ArrayOfNumbers } from "./src/ArrayOfNumbers.ts";
 
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log(ArrayOfNumbers.from([1, 2, 3]).getMissingDigits());
-}
+console.log(ArrayOfNumbers.from([1, 2, 3]).getMissingDigits());
